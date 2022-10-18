fde4bbc901dfb9ff8bae7f24a07d714c40f16452
YubrajKarki <yubrajkarki528@gmail.com>
2022-10-18 10:30:59 +0545

    looping through an array and writing the item in that array to a file

--- FileIO.java
+++ FileIO.java
@@ -4,12 +4,19 @@ import java.io.IOException;
 
 class FileIO {
     public static void main(String[] args) {
+
+        String[] fruits = { "Apple", "Banana", "Mango", "Orange" };
+
         try {
 
             // Using BufferedWriter class to write on a file result.txt
             BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"));
             writer.write("Written on the wall");
             writer.write("\nnew line"); // "\n" allows you to write in new line
+
+            for (String fruit : fruits) {
+                writer.write("\n" + fruit);
+            }
 
             writer.close(); // If we were not to close this, nothing gets written on the file
         } catch (IOException e) {
