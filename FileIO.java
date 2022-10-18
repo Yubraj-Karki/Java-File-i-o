03e24146d0fdbd24f2b9f1a2397580ea820e9d59
YubrajKarki <yubrajkarki528@gmail.com>
2022-10-18 10:28:34 +0545

    Written on the file

--- FileIO.java
+++ FileIO.java
@@ -1,5 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 class FileIO {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        try {
+
+            // Using BufferedWriter class to write on a file result.txt
+            BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"));
+            writer.write("Written on the wall");
+            writer.write("\nnew line"); // "\n" allows you to write in new line
+
+            writer.close(); // If we were not to close this, nothing gets written on the file
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
